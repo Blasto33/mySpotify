f82c00f82a4a34892da53eb3f8a110adec5d0b4d
Thomas de Barros <thomas.de-barros@epitech.eu>
2021-03-15 15:39:56 +0100

    initial push

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import 'semantic-ui-css/semantic.min.css'
+import { Button, Icon } from 'semantic-ui-react'
+
+export default function Home() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
